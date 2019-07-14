cdde3a99f1f74499d1d081f819059783bf3f1261
mirai2333 <973593589@qq.com>
2019-07-14 20:18:56 +0800

    基本结构搭建完成

---
+++ micro/micro-consumer-dept-8000/src/main/java/com/mirai/micro/ConsumerDeptApplication.java
@@ -0,0 +1,11 @@
+package com.mirai.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerDeptApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerDeptApplication.class, args);
+    }
+}
---
+++ micro/micro-consumer-dept-8000/src/main/java/com/mirai/micro/controller/DeptConsumerController.java
@@ -0,0 +1,36 @@
+package com.mirai.micro.controller;
+
+import com.mirai.micro.entity.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/consumer/dept/")
+public class DeptConsumerController {
+    private static final String API_PREFIX = "http://localhost:8001";
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public DeptConsumerController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("add")
+    public boolean addDept(Dept dept) {
+        Boolean result = restTemplate.postForObject(API_PREFIX + "/dept/add", dept, Boolean.class);
+        return result == null ? false : result;
+    }
+
+    @GetMapping("list")
+    public List findAll() {
+        return restTemplate.getForObject(API_PREFIX + "/dept/list", List.class);
+    }
+
+    @GetMapping("get/{deptNo}")
+    public Dept findById(@PathVariable("deptNo") Long deptNo) {
+        return restTemplate.getForObject(API_PREFIX + "/dept/get/" + deptNo, Dept.class);
+    }
+}
