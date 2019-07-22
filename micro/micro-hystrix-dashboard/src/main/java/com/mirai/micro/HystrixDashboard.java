1407c4b09434eb92af92a0a08cc9b51a42b23463
mirai2333 <973593589@qq.com>
2019-07-22 13:29:26 +0800

    hystrix监控和仪表盘监控

--- micro/micro-hystrix-dashboard/src/main/java/com/mirai/micro/HystrixDashboard.java
+++ micro/micro-hystrix-dashboard/src/main/java/com/mirai/micro/HystrixDashboard.java
@@ -2,12 +2,12 @@ package com.mirai.micro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaServerApplication {
+@EnableHystrixDashboard
+public class HystrixDashboard {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(HystrixDashboard.class, args);
     }
 }
--- micro/micro-provider-dept-hystrix-8001/src/main/java/com/mirai/micro/ProviderDeptHystrixApplication.java
+++ micro/micro-provider-dept-hystrix-8001/src/main/java/com/mirai/micro/ProviderDeptHystrixApplication.java
@@ -2,12 +2,12 @@ package com.mirai.micro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableCircuitBreaker
+@EnableHystrix
 public class ProviderDeptHystrixApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProviderDeptHystrixApplication.class, args);
